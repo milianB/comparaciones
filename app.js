eb3e70523fecf117811f9434789ff590b05e8744
Brenda Milian <breen.milian@gmail.com>
2017-10-23 15:27:25 -0500

    ùltimo commit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var comparacion = function(array1, array2) {
-  document.write("funcion declarada, es una prueba")
+  if (array1.length !== array2.length) {
+    document.write("Los arreglos no tienen la misma longitud")
+      return false;
+  }else{
+     for  (var i = 0; i <array1.length; i++) {
+       console.log(array1[i], array2[i]);
+       if(array1[i] !== array2[i]){
+         document.write("los arreglos son diferentes")
+
+        return false;
+       }
+     }
+     return true;
+  }
 }
 
 var arreglo1 = [1,2,3,4,5,6,7,8,9,0]
-var arreglo2 = [1,2,3,4,5,6,7,8,9,0]
+var arreglo2 = [1,2,3,,5,6,7,8,9,0]
 
 comparacion(arreglo1,arreglo2);
