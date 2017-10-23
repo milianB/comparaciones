353c502349cede6f2dd922b33f1cd585d4cf04cf
Brenda Milian <breen.milian@gmail.com>
2017-10-23 15:02:39 -0500

    subiendo carpeta de ejercicios

---
+++ app.js
@@ -0,0 +1,8 @@
+var comparacion = function(array1, array2) {
+  document.write("funcion declarada, es una prueba")
+}
+
+var arreglo1 = [1,2,3,4,5,6,7,8,9,0]
+var arreglo2 = [1,2,3,4,5,6,7,8,9,0]
+
+comparacion(arreglo1,arreglo2);
